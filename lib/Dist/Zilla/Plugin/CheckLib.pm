e406fa252baacd10d01ff4a292afe3541904cd89
Karen Etheridge <ether@cpan.org>
2015-09-05 12:19:19 -0700

    this section is now generated by my weaver bundle

--- lib/Dist/Zilla/Plugin/CheckLib.pm
+++ lib/Dist/Zilla/Plugin/CheckLib.pm
@@ -196,14 +196,6 @@ An additional path to search for headers. Can be used more than once.
 
 A L<ExtUtils::MakeMaker>-style space-separated list of incpaths, each preceded by C<-I>.
 
-=head1 SUPPORT
-
-=for stopwords irc
-
-Bugs may be submitted through L<the RT bug tracker|https://rt.cpan.org/Public/Dist/Display.html?Name=Dist-Zilla-Plugin-CheckLib>
-(or L<bug-Dist-Zilla-Plugin-CheckLib@rt.cpan.org|mailto:bug-Dist-Zilla-Plugin-CheckLib@rt.cpan.org>).
-I am also usually active on irc, as 'ether' at C<irc.perl.org>.
-
 =head1 SEE ALSO
 
 =for :list
